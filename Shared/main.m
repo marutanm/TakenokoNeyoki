0481233e8960eda0be5194c32ff8955965d8226c
marutanm <marutanm@gmail.com>
2010-07-23 00:04:38 +0900

    Initial state

---
+++ Shared/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  TakenokoNeyoki
+//
+//  Created by marutanm on 7/23/10.
+//  Copyright shisobu.in 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	int retVal;
+	if ([[UIDevice currentDevice] respondsToSelector:@selector(userInterfaceIdiom)] && [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
+		retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate_iPad");
+	else
+		retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate_iPhone");        
+    [pool release];
+    return retVal;
+}
---
+++ iPad/AppDelegate_iPad.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate_iPad.h
+//  TakenokoNeyoki
+//
+//  Created by marutanm on 7/23/10.
+//  Copyright shisobu.in 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPad : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@end
+
